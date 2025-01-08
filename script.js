efb7f1802d2292540b2c11176f37548ee3378209
Maksym <example@gmail.com>
2025-01-09 01:57:12 +0200

    add Obj - personal movie DB

--- script.js
+++ script.js
@@ -2,5 +2,11 @@
 
 let numberOfFilms = prompt("Сколько фильмов Вы уже посмотрели?", "");
 
-
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
 
