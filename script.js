e8037c6a06b8da8e08c6b16cd97a54b2bf90f7e0
Maksym <example@gmail.com>
2025-01-14 22:15:04 +0200

    add цицкл to optimizate asking

--- script.js
+++ script.js
@@ -10,12 +10,19 @@ const personalMovieDB = {
     private: false
 };
 
-const movieName = prompt('Один из последних просмотренных фильмов?', 'название фильма');
-const movieRate = prompt('На сколько оцените его?', 'от 0 до 10');
-const movieName1 = prompt('Один из последних просмотренных фильмов?', 'название фильма');
-const movieRate1 = prompt('На сколько оцените его?', 'от 0 до 10');
+// const movieName = prompt('Один из последних просмотренных фильмов?', 'название фильма');
+// const movieRate = prompt('На сколько оцените его?', 'от 0 до 10');
+// const movieName1 = prompt('Один из последних просмотренных фильмов?', 'название фильма');
+// const movieRate1 = prompt('На сколько оцените его?', 'от 0 до 10');
 
-personalMovieDB.movies[movieName] = movieRate;
-personalMovieDB.movies[movieName1] = movieRate1;
+// personalMovieDB.movies[movieName] = movieRate;
+// personalMovieDB.movies[movieName1] = movieRate1;
 
-console.log(personalMovieDB.movies);
+for (let i = 0; i < 2; i++) {
+    const movieName = prompt('Один из последних просмотренных фильмов?', 'название фильма');
+    const movieRate = prompt('На сколько оцените его?', 'от 0 до 10');
+
+    personalMovieDB.movies[movieName] = movieRate;
+}
+
+console.log(personalMovieDB);
